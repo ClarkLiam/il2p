5a52570f5f223fd2f1eff424fad5d89a51948528
Liam Clark <liam.clark@lstudios-media.de>
2025-04-09 19:38:42 +0200

    including files for alternative idea

---
+++ arduino i2c test new/mega2.ino
@@ -0,0 +1,9 @@
+/*Project: IL2P*/
+/*Device: Mega (2)*/
+/*Author: Liam Clark */
+/*Version: 1.8.1 pre-alpha*/
+
+#include <Wire.h>    
+const int SLAVE_UNO = 1;
+const int SLAVE_Mega1 = 2;
+const int SLAVE_Mega2 = 3;
