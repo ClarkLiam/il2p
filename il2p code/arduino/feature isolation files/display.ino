7dd1ee317bd3c8dad361fbcf3f606e3bfebd3f48
Liam Clark <liam.clark@lstudios-media.de>
2025-04-24 17:42:45 +0200

    Update display.ino

--- il2p code/arduino/feature isolation files/display.ino
+++ il2p code/arduino/feature isolation files/display.ino
@@ -3,7 +3,7 @@
 /*Author: Liam Clark */
 /*Version: 1.0.5*/
 
-// Tests a single DigitDisplay connected to the ports 0, 1, 2, 3, 4, 5, 6, as specified in the library
+// Tests a tow DigitDisplay connected to the ports 0, 1, 2, 3, 4, 5, 6, and 7, 8, 9, 10, 11, 12, 13 as specified in the library
 
 #include <digitdisplay.h>
 
@@ -12,31 +12,43 @@
 
 // Declare digitDisplay as a global variable
 DigitDisplay digitDisplay1(0, 1, 2, 3, 4, 5, 6);
+DigitDisplay digitDisplay2(7, 8, 9, 10, 11, 12, 13);
 
 
 void setup() {
     digitDisplay1.begin();
+    digitDisplay2.begin();
 }
 void loop(){
 
     digitDisplay1.displayNumber(0);
+    digitDisplay2.displayNumber(9);
     delay(1000);
     digitDisplay1.displayNumber(1);
+    digitDisplay2.displayNumber(8);
     delay(1000);
     digitDisplay1.displayNumber(2);
+    digitDisplay2.displayNumber(7);
     delay(1000);
     digitDisplay1.displayNumber(3);
+    digitDisplay2.displayNumber(6);
     delay(1000);
     digitDisplay1.displayNumber(4);
+    digitDisplay2.displayNumber(5);
     delay(1000);
     digitDisplay1.displayNumber(5);
+    digitDisplay2.displayNumber(4);
     delay(1000);
     digitDisplay1.displayNumber(6);
+    digitDisplay2.displayNumber(3);
     delay(1000);
     digitDisplay1.displayNumber(7);
+    digitDisplay2.displayNumber(2);
     delay(1000);
     digitDisplay1.displayNumber(8);
+    digitDisplay2.displayNumber(1);
     delay(1000);
     digitDisplay1.displayNumber(9);
+    digitDisplay2.displayNumber(0);
     delay(1000);
 }
