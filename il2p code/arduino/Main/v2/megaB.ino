b95df0a5453866658453a26743b6e65cd9f31ae8
Liam Clark <liam.clark@lstudios-media.de>
2025-12-09 11:15:47 +0100

    intial commit

---
+++ il2p code/arduino/Main/v2/megaB.ino
@@ -0,0 +1,11 @@
+/*Project: illumination2PRO*/
+/*Device: Arduino_Mega.B*/
+/*Author: Liam Clark */
+/*Version: 2.0.0 pre-alpha*/
+
+#include <Wire.h>
+
+const int SLAVE_MASTER = 1;
+const int SLAVE_MegaA = 2;
+const int SLAVE_MegaB = 3;
+const int SLAVE_Display = 4;
---
+++ il2p code/arduino/Main/v2/unoDisplay.ino
@@ -0,0 +1,11 @@
+/*Project: illumination2PRO*/
+/*Device: Arduino_Uno.Display*/
+/*Author: Liam Clark */
+/*Version: 2.0.0 pre-alpha*/
+
+#include <Wire.h>
+
+const int SLAVE_MASTER = 1;
+const int SLAVE_MegaA = 2;
+const int SLAVE_MegaB = 3;
+const int SLAVE_Display = 4;
